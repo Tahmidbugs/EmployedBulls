d801a9bd8c3f2751140d9a4e29c4449b8a60e6a7
Musa Al Sathman Gazi <gazi@usf.edu>
2023-03-17 20:13:28 -0400

    akdjak

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,35 @@
-import Login from "./Pages/Login";
-import RecruiterReg from "./Pages/RecruiterSide/RecruiReg/RecruiterReg";
-import Registration from "./Pages/Registration";
-import RecruiterJobFeed from "./Pages/RecruiterSide/RecruiReg/RecruiterJobFeed";
-import StudentAppForm from "./Pages/StudentSide/StudentAppForm";
-import JobPage from "./Pages/RecruiterSide/RecruiReg/JobPage";
-import Dashboard from "./Pages/RecruiterSide/RecruiReg/Dashboard";
-import JobAdded from "./Pages/RecruiterSide/Dashboard Components/JobAdded";
-import InboxWithStudentMessages from "./Pages/RecruiterSide/Dashboard Components/InboxWithStudentMessages";
+import Login from './Pages/Login';
+import RecruiterReg from './Pages/RecruiterSide/RecruiReg/RecruiterReg';
+import Registration from './Pages/Registration';
+import RecruiterJobFeed from './Pages/RecruiterSide/RecruiReg/RecruiterJobFeed';
+import StudentAppForm from './Pages/StudentSide/StudentAppForm';
+import JobPage from './Pages/RecruiterSide/RecruiReg/JobPage';
+import Dashboard from './Pages/RecruiterSide/RecruiReg/Dashboard';
+import JobAdded from './Pages/RecruiterSide/Dashboard Components/JobAdded';
+import InboxWithStudentMessages from './Pages/RecruiterSide/Dashboard Components/InboxWithStudentMessages';
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
-import StudentAppForm from "./Pages/StudentSide/StudentAppForm";
-import JobFeed from "./Pages/StudentSide/JobFeed";
-import StudentProfile from "./Pages/StudentSide/StudentProfile";
-import RecruiterJobFeed from "./Pages/RecruiterSide/RecruiReg/RecruiterJobFeed";
-
+} from 'react-router-dom';
+import JobFeed from './Pages/StudentSide/JobFeed';
 function App() {
   return (
-    <div className="body">
+    <div className='body'>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/recruiter-reg" element={<RecruiterReg />} />
-          <Route path="/student-reg" element={<StudentAppForm />} />
-          <Route path="/recJobFeed" element={<RecruiterJobFeed />} />
-          <Route path="/jobPageRec/:id" element={<JobPage />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/JobAdded" element={<JobAdded />} />
-          <Route path="/InboxRec" element={<InboxWithStudentMessages />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/registration' element={<Registration />} />
+          <Route path='/recruiter-reg' element={<RecruiterReg />} />
+          <Route path='/student-reg' element={<StudentAppForm />} />
+          <Route path='/recJobFeed' element={<RecruiterJobFeed />} />
+          <Route path='/jobPageRec/:id' element={<JobPage />} />
+          <Route path='/Dashboard' element={<Dashboard />} />
+          <Route path='/JobAdded' element={<JobAdded />} />
+          <Route path='/JobFeed' element={<JobFeed />} />
+          <Route path='/InboxRec' element={<InboxWithStudentMessages />} />
         </Routes>
       </Router>
     </div>
