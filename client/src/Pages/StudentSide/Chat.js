2c8ed396bd53397fa8da7d31dff22134455aaca5
Musa Al Sathman Gazi <gazi@usf.edu>
2023-03-18 10:36:17 -0400

    added Socket.io features

--- client/src/Pages/StudentSide/Chat.js
+++ client/src/Pages/StudentSide/Chat.js
@@ -1,62 +1,77 @@
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
-const socket = io("http://localhost:8800", {
+const socket = io('http://localhost:8800', {
   withCredentials: false,
 });
 
 const Chat = ({ senderId = 12, receiverId = 21 }) => {
   const [chatHistory, setChatHistory] = useState([]);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     // retrieve chat history from server
-    socket.emit("get_chat_history", { senderId, receiverId });
+    socket.emit('get_chat_history', { senderId, receiverId });
 
     // receive chat history from server
-    socket.on("chat_history", (history) => {
+    socket.on('chat_history', history => {
       setChatHistory(history);
     });
 
     return () => {
       //   cleanup;
-      socket.off("chat_history");
-      socket.off("message");
+      socket.off('chat_history');
+      socket.off('message');
     };
   }, [senderId, receiverId]);
 
-  const handleSendMessage = (e) => {
+  const handleSendMessage = e => {
     e.preventDefault();
 
     // send message to server
-    socket.emit("message", { senderId, receiverId, message });
+    socket.emit('message', { senderId, receiverId, message });
 
     // add message to chat history
-    setChatHistory((prevHistory) => [
+    setChatHistory(prevHistory => [
       ...prevHistory,
       { senderId, receiverId, message },
     ]);
 
     // clear input field
-    setMessage("");
+    setMessage('');
   };
 
   return (
     <div>
       <ul>
-        {chatHistory.map((messageObj, index) => (
-          <li key={index}>
-            {messageObj.senderId}: {messageObj.message}
-          </li>
-        ))}
+        {chatHistory.map((msg, index) => {
+          console.log('sender is:', msg.sender_id, 'sender now', senderId);
+          return (
+            <li
+              key={index}
+              style={{
+                backgroundColor: `${
+                  msg.sender_id == senderId
+                    ? 'blue'
+                    : msg.sender_id
+                    ? 'green'
+                    : 'blue'
+                }`,
+                padding: 20,
+              }}
+            >
+              {msg.message}
+            </li>
+          );
+        })}
       </ul>
       <form onSubmit={handleSendMessage}>
         <input
-          type="text"
+          type='text'
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={e => setMessage(e.target.value)}
         />
-        <button type="submit">Send</button>
+        <button type='submit'>Send</button>
       </form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-const cors = require("cors");
-const authRoute = require("./Routes/Auth");
-const jobRoute = require("./Routes/Job");
-const http = require("http").createServer(app);
-const db = require("./db");
+const dotenv = require('dotenv');
+const cors = require('cors');
+const authRoute = require('./Routes/Auth');
+const jobRoute = require('./Routes/Job');
+const http = require('http').createServer(app);
+const db = require('./db');
 
 dotenv.config();
 
@@ -15,72 +15,74 @@ app.use(express.json());
 // cors config-- allows backend frontend communication
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
     optionsSuccessStatus: 200,
     credentials: true,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-      "Access-Control-Allow-Headers": "Content-Type",
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type',
     },
   })
 );
 
-const io = require("socket.io")(http, {
+const io = require('socket.io')(http, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     // credentials: true,
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+io.on('connection', socket => {
+  console.log('User connected');
 
   // retrieve chat history from PostgreSQL database
-  socket.on("get_chat_history", ({ senderId, receiverId }) => {
+  socket.on('get_chat_history', ({ senderId, receiverId }) => {
+    console.log('reading');
     db.query(
-      "SELECT * FROM chat_messages WHERE (sender_id = $1 AND receiver_id = $2) OR (sender_id = $2 AND receiver_id = $1) ORDER BY created_at",
+      'SELECT * FROM chat_messages WHERE (sender_id = $1 AND receiver_id = $2) OR (sender_id = $2 AND receiver_id = $1) ORDER BY created_at',
       [senderId, receiverId],
       (err, res) => {
         if (err) {
           console.log(err);
         } else {
           // send chat history to client
-          socket.emit("chat_history", res.rows);
+          console.log(res.rows);
+          socket.emit('chat_history', res.rows);
         }
       }
     );
   });
 
-  socket.on("message", ({ senderId, receiverId, message }) => {
-    console.log("Received message:", message);
+  socket.on('message', ({ senderId, receiverId, message }) => {
+    console.log('Received message:', message);
 
     // store message in PostgreSQL database
     db.query(
-      "INSERT INTO chat_messages (sender_id, receiver_id, message) VALUES ($1, $2, $3)",
+      'INSERT INTO chat_messages (sender_id, receiver_id, message) VALUES ($1, $2, $3)',
       [senderId, receiverId, message],
       (err, res) => {
         if (err) {
           console.log(err);
         } else {
           // send message to all clients
-          io.emit("message", { senderId, receiverId, message });
+          io.emit('message', { senderId, receiverId, message });
         }
       }
     );
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 });
 
 // use routes
-app.use("/api/auth", authRoute);
-app.use("/api/job", jobRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/job', jobRoute);
 
 //twilio config
 
