90ec8c9cc181fba43656c5c5b9a9661bc06311ea
Abdur Rahman Bin Sharif <asharif1@usf.edu>
2023-04-26 05:00:52 -0400

    dashboard styling and explore companies page

--- client/src/Pages/StudentSide/StudentDashboard.js
+++ client/src/Pages/StudentSide/StudentDashboard.js
@@ -1,71 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Nav from './Navbar';
 
+
 function StudentDashboard() {
-return (
-<div style={{ display: 'flex' }}>
-<Nav />
-    
-<div style={{ 
-      marginLeft: '50px', 
-      padding: '20px', 
-      backgroundColor: '#f2f2f2', 
-      width: '100%' 
-    }}>
-    <nav>
-      <div>
-        <button style={{ 
-            backgroundColor: '#FF2400', 
-            color: 'white', 
-            border: 'none', 
-            borderRadius: '5px', 
-            padding: '10px', 
-            margin: '5px' 
-          }}>My Applications</button>
-        <button style={{ 
-            backgroundColor: '#FF2400', 
-            color: 'white', 
-            border: 'none', 
-            borderRadius: '5px', 
-            padding: '10px', 
-            margin: '5px' 
-          }}>Messages</button>
-        <button style={{ 
-            backgroundColor: '#FF2400', 
-            color: 'white', 
-            border: 'none', 
-            borderRadius: '5px', 
-            padding: '10px', 
-            margin: '5px' 
-          }}><Link to="/jobfeed" style={{ 
-              color: 'white' 
-            }}>Job Search</Link></button>
-        <button style={{ 
-            backgroundColor: '#FF2400', 
-            color: 'white', 
-            border: 'none', 
-            borderRadius: '5px', 
-            padding: '10px', 
-            margin: '5px' 
-          }}>Explore Companies</button>
-        <button style={{ 
-            backgroundColor: '#FF2400', 
-            color: 'white', 
-            border: 'none', 
-            borderRadius: '5px', 
-            padding: '10px', 
-            margin: '5px' 
-          }}><Link to="/studentprofile" style={{ 
-              color: 'white' 
-            }}>Profile Settings</Link></button>
-      </div>
-    </nav>
-    <div className="count-box">
-      <p>Total Jobs Applied: 10</p>
-      <p>Total Interviews: 3</p>
-    </div>
-    <div className="upcoming-box" style={{ 
+  const [totalJobsApplied, setTotalJobsApplied] = useState(0);
+  const [totalInterviews, setTotalInterviews] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTotalJobsApplied((prevCount) => prevCount + 1);
+    }, 200);
+
+    if (totalJobsApplied === 10) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [totalJobsApplied]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTotalInterviews((prevCount) => prevCount + 1);
+    }, 400);
+
+    if (totalInterviews === 3) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [totalInterviews]);
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <Nav />
+
+      <div style={{ 
+        marginLeft: '50px', 
+        padding: '20px', 
+        backgroundColor: '#f2f2f2', 
+        width: '100%' 
+      }}>
+        <nav>
+          <div>
+            <button style={{ 
+                backgroundColor: '#FF2400', 
+                color: 'white', 
+                border: 'none', 
+                borderRadius: '5px', 
+                padding: '10px', 
+                margin: '5px' 
+              }}>My Applications</button>
+            <button style={{ 
+                backgroundColor: '#FF2400', 
+                color: 'white', 
+                border: 'none', 
+                borderRadius: '5px', 
+                padding: '10px', 
+                margin: '5px' 
+              }}>Messages</button>
+            <button style={{ 
+                backgroundColor: '#FF2400', 
+                color: 'white', 
+                border: 'none', 
+                borderRadius: '5px', 
+                padding: '10px', 
+                margin: '5px' 
+              }}><Link to="/jobfeed" style={{ 
+                  color: 'white' 
+                }}>Job Search</Link></button>
+            <button style={{ 
+                backgroundColor: '#FF2400', 
+                color: 'white', 
+                border: 'none', 
+                borderRadius: '5px', 
+                padding: '10px', 
+                margin: '5px' 
+              }}><Link to="/explorecompanies" style={{ 
+                color: 'white' 
+              }}>Explore Companies</Link></button>
+            <button style={{ 
+                backgroundColor: '#FF2400', 
+                color: 'white', 
+                border: 'none', 
+                borderRadius: '5px', 
+                padding: '10px', 
+                margin: '5px' 
+              }}><Link to="/studentprofile" style={{ 
+                  color: 'white' 
+                }}>Profile Settings</Link></button>
+          </div>
+        </nav>
+        <div className="count-box" style={{ textAlign: "center", animation: "count-up 2s forwards" }}>
+          <p style={{ fontSize: "24px", fontWeight: "bold", margin: "0" }}>
+            Total Jobs Applied: {totalJobsApplied}
+          </p>
+          <p style={{ fontSize: "24px", fontWeight: "bold", margin: "0" }}>
+Total Interviews: {totalInterviews}
+</p>
+</div>
+<div className="upcoming-box" style={{ 
         backgroundColor: 'white', 
         padding: '10px', 
         borderRadius: '5px', 
@@ -108,3 +142,8 @@ return (
 );
 }
 export default StudentDashboard;
+
+
+
+
+
