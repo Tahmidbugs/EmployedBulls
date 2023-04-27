2c9f159ec0de43ce7eb727d1ba746d2a2cd95b60
Musa Al Sathman Gazi <gazi@usf.edu>
2023-04-27 05:31:45 -0400

    Worked on inbox frontend

--- client/src/Pages/RecruiterSide/Dashboard Components/InboxWithStudentMessages.js
+++ client/src/Pages/RecruiterSide/Dashboard Components/InboxWithStudentMessages.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './dashboardstyle.css';
 import Chat from '../../StudentSide/Chat';
 import Nav from './Navbar';
 function InboxWithStudentMessages() {
+  const [activeChat, setActiveChat] = useState(null);
+
+  const handleStudentClick = receiverId => {
+    setActiveChat(receiverId);
+  };
+
+  const studentProfilePictures = {
+    21: 'https://img.freepik.com/premium-vector/portrait-beautiful-young-woman-with-short-wavy-hair_478440-368.jpg',
+    22: 'https://scontent.ftpa1-2.fna.fbcdn.net/v/t31.18172-8/15732054_1829744933948321_6912147507579640372_o.png?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=B32qzT7yVPUAX-fjQ1F&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfDK8QeAwi9cQRERIju8M27Ftyr8N0FSPleOzipc5SHZJA&oe=6471C0BB',
+  };
+
   return (
-    <div style={{ display: 'flex' }}>
+    <div
+      style={{
+        display: 'flex',
+        backgroundColor: '#F5F5F5',
+        backgroundImage:
+          'url(https://www.transparenttextures.com/patterns/batthern.png)',
+      }}
+    >
       <Nav />
       <div className='inbox'>
-        <div className='dashboard'>
-          <h2>Inbox with Student Messages</h2>
+        <div
+          className='dashboard'
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginRight: '-60%',
+          }}
+        >
+          <h1 style={{ color: '#FF3953' }}>Inbox with Student Messages</h1>
           {/* Add table for displaying student messages here */}
           <table>
             <thead>
@@ -20,16 +45,42 @@ function InboxWithStudentMessages() {
             </thead>
             <tbody>
               <tr>
-                <td>John Doe</td>
+                <td>
+                  <img
+                    src={studentProfilePictures[22]}
+                    alt='John Doe Profile'
+                    width='50'
+                  />
+                  <a href='#' onClick={() => handleStudentClick(21)}>
+                    John Doe
+                  </a>
+                </td>
                 <td className='inbox__message'>
-                  <Chat senderId={12} receiverId={21} />
+                  <Chat
+                    senderId={12}
+                    receiverId={21}
+                    active={activeChat === 21}
+                  />
                 </td>
                 <td>2023-03-16</td>
               </tr>
               <tr>
-                <td>Jane Smith</td>
+                <td>
+                  <img
+                    src={studentProfilePictures[21]}
+                    alt='Jane Smith Profile'
+                    width='50'
+                  />
+                  <a href='#' onClick={() => handleStudentClick(22)}>
+                    Jane Smith
+                  </a>
+                </td>
                 <td className='inbox__message'>
-                  <Chat senderId={12} receiverId={22} />
+                  <Chat
+                    senderId={12}
+                    receiverId={22}
+                    active={activeChat === 22}
+                  />
                 </td>
                 <td>2023-03-15</td>
               </tr>
@@ -40,5 +91,4 @@ function InboxWithStudentMessages() {
     </div>
   );
 }
-
 export default InboxWithStudentMessages;
--- client/src/Pages/RecruiterSide/RecruiReg/Dashboard.js
+++ client/src/Pages/RecruiterSide/RecruiReg/Dashboard.js
@@ -1,20 +1,19 @@
-import React, { useContext, useState } from "react";
-import RecruiterJobFeed from "./RecruiterJobFeed";
-import InboxWithStudentMessages from "../Dashboard Components/InboxWithStudentMessages";
-import UpcomingInterviews from "../Dashboard Components/UpcomingInterviews";
-import "../Dashboard Components/dashboardstyle.css";
-import Nav from "../Dashboard Components/Navbar";
-import { BiBell } from "react-icons/bi";
-import { FaUserCircle } from "react-icons/fa";
-import { AuthContext } from "../../../Context/AuthContext";
-import AnimatonData from "../../../Assets/Lotties/Add.json";
-import Lottie from "react-lottie";
-import { Bars } from "react-loader-spinner";
+import React, { useContext, useState } from 'react';
+import RecruiterJobFeed from './RecruiterJobFeed';
+import InboxWithStudentMessages from '../Dashboard Components/InboxWithStudentMessages';
+import UpcomingInterviews from '../Dashboard Components/UpcomingInterviews';
+import '../Dashboard Components/dashboardstyle.css';
+import Nav from '../Dashboard Components/Navbar';
+import { BiBell } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
+import { AuthContext } from '../../../Context/AuthContext';
+import AnimatonData from '../../../Assets/Lotties/Add.json';
+import Lottie from 'react-lottie';
+import { Bars } from 'react-loader-spinner';
 
-
-import axios from "axios";
-import AddJobModal from "../Dashboard Components/AddJobModal";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import AddJobModal from '../Dashboard Components/AddJobModal';
+import { Link } from 'react-router-dom';
 function Dashboard() {
   const [addedJobs, setAddedJobs] = React.useState([]);
   const { user, dispatch } = useContext(AuthContext);
@@ -22,28 +21,28 @@ function Dashboard() {
   //fetch added jobs by recruiter user.email
   React.useEffect(() => {
     axios
-      .get("http://localhost:8800/api/job/getJobsByRecruiter", {
+      .get('http://localhost:8800/api/job/getJobsByRecruiter', {
         params: {
           email: user.email,
         },
       })
-      .then((res) => {
+      .then(res => {
         setAddedJobs(res.data);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, []);
 
   return (
-    <div style={{ display: "flex", minHeight: "100vh" }}>
+    <div style={{ display: 'flex', minHeight: '100vh' }}>
       <Nav />
-      <div style={{ height: "100vh" }}>
+      <div style={{ height: '100vh' }}>
         <div
-          className="dashboard-header"
-          style={{ height: "100vh", width: "80%" }}
+          className='dashboard-header'
+          style={{ height: '100vh', width: '80%' }}
         >
-          <div className="header-right">
+          <div className='header-right'>
             <span>Recruiter Name</span>
             <FaUserCircle size={30} style={{ marginLeft: 10 }} />
             <BiBell size={30} style={{ marginLeft: 10 }} />
@@ -56,39 +55,39 @@ function Dashboard() {
           <br />
           <br />
 
-          <div style={{ display: "flex", justifyContent: "space-around" }}>
-            <div style={{ width: "33%" }}>
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <div style={{ width: '33%' }}>
               <h5>Add a new job</h5>
               <AddJob />
             </div>
 
-            <div className="dashboard__section dashboard__section--inbox">
+            <div className='dashboard__section dashboard__section--inbox'>
               <h2>Inbox</h2>
               <LatestMessages />
               {/* <InboxWithStudentMessages /> */}
             </div>
-            <div className="dashboard__section dashboard__section--upcoming-interviews">
+            <div className='dashboard__section dashboard__section--upcoming-interviews'>
               <h2>Upcoming Interviews</h2>
               <UpcomingInterviews />
             </div>
           </div>
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-around",
-              boxShadow: "none",
+              display: 'flex',
+              justifyContent: 'space-around',
+              boxShadow: 'none',
               borderRadius: 0,
             }}
           >
-            <div className="dashboard__section dashboard__section--job-added">
+            <div className='dashboard__section dashboard__section--job-added'>
               <h2>Job Summary</h2>
               {/* job list added by recruiter */}
               <Last2jobs addedJobs={addedJobs} />
             </div>
 
-            <div className="dashboard__section dashboard__section--job-added">
+            <div className='dashboard__section dashboard__section--job-added'>
               <h2>Applicants</h2>
-              <Link to="/studentRecView"> View Applicants</Link>
+              <Link to='/studentRecView'> View Applicants</Link>
               {/* <JobAdded /> */}
             </div>
           </div>
@@ -106,7 +105,7 @@ const AddJob = () => {
     autoplay: true,
     animationData: AnimatonData,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
+      preserveAspectRatio: 'xMidYMid slice',
     },
   };
 
@@ -115,14 +114,14 @@ const AddJob = () => {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <p>You have added {} jobs</p>
-      <button className="btn btn--primary" onClick={() => setJobModal(true)}>
+      <button className='btn btn--primary' onClick={() => setJobModal(true)}>
         <Lottie options={defaultOptions} height={100} width={100} />
       </button>
       <p>Add a new job</p>
@@ -144,7 +143,7 @@ const Last2jobs = ({ addedJobs }) => {
           </tr>
         </thead>
         <tbody>
-          {addedJobs.map((job) => (
+          {addedJobs.map(job => (
             <tr key={job._id}>
               <td>{job.company_name}</td>
               <td>{job.position_name}</td>
@@ -161,20 +160,20 @@ const LatestMessages = () => {
   const [messages, setMessages] = useState([
     {
       id: 1,
-      name: "John Doe",
-      message: "Hi, I am interested in the Software Engineer position.",
-      date: "2023-03-16",
+      name: 'John Doe',
+      message: 'Hi, I am interested in the Software Engineer position.',
+      date: '2023-03-16',
     },
     {
       id: 2,
-      name: "Jane Smith",
-      message: "Hi, I am interested in the Product Manager position.",
-      date: "2023-03-15",
+      name: 'Jane Smith',
+      message: 'Hi, I am interested in the Product Manager position.',
+      date: '2023-03-15',
     },
   ]);
 
   return (
-    <div className="inbox">
+    <div className='inbox'>
       <table>
         <thead>
           <tr>
@@ -184,10 +183,10 @@ const LatestMessages = () => {
           </tr>
         </thead>
         <tbody>
-          {messages.map((message) => (
+          {messages.map(message => (
             <tr key={message.id}>
               <td>{message.name}</td>
-              <td className="inbox__message">{message.message}</td>
+              <td className='inbox__message'>{message.message}</td>
               <td>{message.date}</td>
             </tr>
           ))}
@@ -201,22 +200,22 @@ const RecentlyAddedJobs = () => {
   const [jobs, setJobs] = useState([
     {
       id: 1,
-      title: "Software Engineer",
-      company: "Google",
-      location: "Mountain View, CA",
-      date: "2023-03-16",
+      title: 'Software Engineer',
+      company: 'Google',
+      location: 'Mountain View, CA',
+      date: '2023-03-16',
     },
     {
       id: 2,
-      title: "Product Manager",
-      company: "Facebook",
-      location: "Menlo Park, CA",
-      date: "2023-03-15",
+      title: 'Product Manager',
+      company: 'Facebook',
+      location: 'Menlo Park, CA',
+      date: '2023-03-15',
     },
   ]);
 
   return (
-    <div className="recruiter-job-feed">
+    <div className='recruiter-job-feed'>
       <table>
         <thead>
           <tr>
@@ -227,7 +226,7 @@ const RecentlyAddedJobs = () => {
           </tr>
         </thead>
         <tbody>
-          {jobs.map((job) => (
+          {jobs.map(job => (
             <tr key={job.id}>
               <td>{job.title}</td>
               <td>{job.company}</td>
